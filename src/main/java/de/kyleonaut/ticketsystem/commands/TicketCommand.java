ca43da17f284bf1dd2a76d102b70f168d8057cb7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-04 22:48:14 +0100

    Upload files via Upload

--- src/main/java/de/kyleonaut/ticketsystem/commands/TicketCommand.java
+++ src/main/java/de/kyleonaut/ticketsystem/commands/TicketCommand.java
@@ -1,21 +1,10 @@
 package de.kyleonaut.ticketsystem.commands;
 
-import de.kyleonaut.ticketsystem.TicketSystem;
-import de.kyleonaut.ticketsystem.util.Builder;
-import de.kyleonaut.ticketsystem.util.Config;
 import de.kyleonaut.ticketsystem.util.inventory;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.scheduler.BukkitRunnable;
-
-
-import java.util.ArrayList;
 
 public class TicketCommand implements CommandExecutor {
 
