f46e18574d0de180e3ef2c4cf9ec65f8e233b187
BuildTools <unconfigured@null.spigotmc.org>
2021-01-05 00:10:42 +0100

    Add files via Upload

--- src/main/java/de/kyleonaut/ticketsystem/util/TicketSqlAPI.java
+++ src/main/java/de/kyleonaut/ticketsystem/util/TicketSqlAPI.java
@@ -1,10 +1,12 @@
 package de.kyleonaut.ticketsystem.util;
 
 import de.kyleonaut.ticketsystem.TicketSystem;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.UUID;
 
 public class TicketSqlAPI {
 
@@ -13,6 +15,25 @@ public class TicketSqlAPI {
     }
 
     public static void changeStatus(Status status, int index, Player player) throws SQLException {
-        TicketSystem.getCon().execute("UPDATE ticketsystem_tickets SET ticket_status = '" + status + "',  ");
+        TicketSystem.getCon().execute("UPDATE ticketsystem_tickets SET ticket_status = '" + status + "', moderator_uuid = '" + player.getUniqueId() + "' WHERE index = " + index + "");
     }
+
+    public static Player getPlayerByIndex(int index) throws SQLException {
+        UUID player_uuid = UUID.fromString((String) TicketSystem.getCon().get("SELECT uuid_player FROM ticketsystem_tickets WHERE index = " + index + "", "uuid_player").get(0));
+        return Bukkit.getPlayer(player_uuid);
+    }
+
+    public static TicketTypes getTicketTypeByIndex(int index) throws SQLException {
+        return (TicketTypes) TicketSystem.getCon().get("SELECT ticket_type FROM ticketsystem_ticktes WHERE index = " + index + "", "ticket_type").get(0);
+    }
+
+    public static String getArgsByIndex(int index) throws SQLException {
+        return (String) TicketSystem.getCon().get("SELECT ticket_args FROM ticketsystem_tickets WHERE index = " + index + "", "ticket_args").get(0);
+    }
+
+    public static Date getEingangDatumByIndex(int index) throws SQLException {
+        return (Date) TicketSystem.getCon().get("SElECT eingangs_datum FROM ticketsytem_tickets WHERE index = " + index, "eingangs_datum").get(0);
+    }
+
+
 }
