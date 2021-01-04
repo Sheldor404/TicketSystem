3f3f8f18c950b0392b903fe1c5263174ab734800
BuildTools <unconfigured@null.spigotmc.org>
2021-01-04 22:52:12 +0100

    Add files via Upload

--- src/main/java/de/kyleonaut/ticketsystem/util/InventoryHolder.java
+++ src/main/java/de/kyleonaut/ticketsystem/util/InventoryHolder.java
@@ -1,17 +1,13 @@
 package de.kyleonaut.ticketsystem.util;
 
-import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class InventoryHolder {
-    static int id = 15;
-    static Material empty = Material.STAINED_GLASS_PANE;
+
 
     public static void openKategorienGui(Player p) {
         Builder builder = new Builder();
