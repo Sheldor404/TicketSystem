f0e805a10b302498edfc8e43ab2582f2e6fdb395
BuildTools <unconfigured@null.spigotmc.org>
2021-01-04 01:15:54 +0100

    Add files via Upload

---
+++ src/main/java/de/kyleonaut/ticketsystem/TicketSystem.java
@@ -0,0 +1,11 @@
+package de.kyleonaut.ticketsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TicketSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
