5a06a9fede43ee7b7d87bbb5705c046c195332eb
BuildTools <unconfigured@null.spigotmc.org>
2021-01-27 21:54:50 +0100

    Add files via Upload

--- src/main/java/de/kyleonaut/ticketsystem/events/HandlePlayerJoin.java
+++ src/main/java/de/kyleonaut/ticketsystem/events/HandlePlayerJoin.java
@@ -4,7 +4,6 @@ import de.kyleonaut.ticketsystem.util.Config;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class HandlePlayerJoin implements Listener {
